c5097c2a2a91055028cac4ac4fd3ba331e0a8fb3
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-04-05 13:20:30 -0400

    debugging

--- stepper_Stuff/stepper.c
+++ stepper_Stuff/stepper.c
@@ -3,10 +3,35 @@
 #define LOW 0
 #define INPUT 0
 #define OUTPUT 1
+#define PIN0_SET_OUTPUT __asm__( "addi $1 $1 1""sw $1 12288($0)");
+#define PIN1_SET_OUTPUT __asm__( "addi $1 $1 1""sw $1 12289($0)");
+#define PIN2_SET_OUTPUT __asm__( "addi $1 $1 1""sw $1 12290($0)");
+#define PIN3_SET_OUTPUT __asm__( "addi $1 $1 1""sw $1 12291($0)");
 
+#define PIN0_SET_HIGH __asm__( "addi $1 $1 0""sw $1 4096($0)");
+#define PIN0_SET_LOW __asm__( "sw $0 4096($0)");
+#define PIN1_SET_HIGH __asm__( "addi $1 $1 0""sw $1 4097($0)");
+#define PIN1_SET_LOW __asm__( "sw $0 4097($0)");
+#define PIN2_SET_HIGH __asm__( "addi $1 $1 0""sw $1 4098($0)");
+#define PIN2_SET_LOW __asm__( "sw $0 4098($0)");
+#define PIN3_SET_HIGH __asm__( "addi $1 $1 0""sw $1 4099($0)");
+#define PIN3_SET_LOW __asm__( "sw $0 4099($0)");
 
+// int i = 3;
 
-int numSteps = 4; // Number of steps in the sequence
+// PIN0_SET_OUTPUT
+// PIN0_SET_HIGH
+
+// PIN1_SET_OUTPUT
+// PIN1_SET_HIGH
+
+// PIN2_SET_OUTPUT
+// PIN2_SET_HIGH
+
+// PIN3_SET_OUTPUT
+// PIN3_SET_HIGH
+
+// int numSteps = 4; // Number of steps in the sequence
 // Define the sequence of steps for the stepper motor
 // const byte stepSequence4[4] = {
 //   B1100, // Step A: AB
@@ -22,8 +47,8 @@ int numSteps = 4; // Number of steps in the sequence
 //   B1001  // Step D: DA
 // };
 
-int stepSequence = 37740; // See above
-int pins[4] = {0, 1, 2, 3};
+// int stepSequence = 37740; // See above
+// int pins[4] = {0, 1, 2, 3};
 
 
 void pinMode(int pin, int mode){
@@ -38,67 +63,78 @@ void digitalWrite(int pin, int level){
         "sw (level) 0((addr))"
     );
 }
-void digitalRead(int pin){
-    int addr = 4096 + pin; // 2^12 + pin num
-    __asm__(
-        "lw $v0 0((addr))"
-    );
-}
 
-void setup() {
-  // Set coil pins as outputs
-    pinMode(0,OUTPUT);
-    pinMode(1,OUTPUT);
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
+pinMode(0, 1);
+digitalWrite(0, 1);
+pinMode(1, 1);
+digitalWrite(1, 1);
+pinMode(2, 1);
+digitalWrite(2, 1);
+pinMode(3, 1);
+digitalWrite(3, 1);
 
-    digitalWrite(0,LOW);
-    digitalWrite(1,LOW);
-    digitalWrite(2,LOW);
-    digitalWrite(3,LOW);
-}
+// void digitalRead(int pin){
+//     int addr = 4096 + pin; // 2^12 + pin num
+//     __asm__(
+//         "lw $v0 0((addr))"
+//     );
+// }
 
-void wait(int cycles){
-    for (int i = 0; i < cycles; i += 1){}
-}
+// void setup() {
+//   // Set coil pins as outputs
+//     pinMode(0,OUTPUT);
+//     pinMode(1,OUTPUT);
+//     pinMode(2,OUTPUT);
+//     pinMode(3,OUTPUT);
 
-void stepMotor(int step, int* pins) {
-  // Output the step sequence
-    int mod4 = step & 3;
-    if (mod4 == 0) {
-        digitalWrite(pins[0], HIGH);
-        digitalWrite(pins[1], LOW);
-        digitalWrite(pins[2], LOW);
-        digitalWrite(pins[3], HIGH);
-    } else if (mod4 == 1) {
-        digitalWrite(pins[0], LOW);
-        digitalWrite(pins[1], HIGH);
-        digitalWrite(pins[2], LOW);
-        digitalWrite(pins[3], HIGH);
-    } else if (mod4 == 2) {
-        digitalWrite(pins[0], LOW);
-        digitalWrite(pins[1], HIGH);
-        digitalWrite(pins[2], HIGH);
-        digitalWrite(pins[3], LOW);
-    } else {
-        digitalWrite(pins[0], HIGH);
-        digitalWrite(pins[1], LOW);
-        digitalWrite(pins[2], HIGH);
-        digitalWrite(pins[3], LOW);
-    }
-}
+//     digitalWrite(0,LOW);
+//     digitalWrite(1,LOW);
+//     digitalWrite(2,LOW);
+//     digitalWrite(3,LOW);
+// }
 
-int main(){
+// void wait(int cycles){
+//     for (int i = 0; i < cycles; i += 1){}
+// }
 
-   setup(); //init IO
+// void stepMotor(int step, int* pins) {
+//   // Output the step sequence
+//     int mod4 = step & 3;
+//     if (mod4 == 0) {
+//         digitalWrite(pins[0], HIGH);
+//         digitalWrite(pins[1], LOW);
+//         digitalWrite(pins[2], LOW);
+//         digitalWrite(pins[3], HIGH);
+//     } else if (mod4 == 1) {
+//         digitalWrite(pins[0], LOW);
+//         digitalWrite(pins[1], HIGH);
+//         digitalWrite(pins[2], LOW);
+//         digitalWrite(pins[3], HIGH);
+//     } else if (mod4 == 2) {
+//         digitalWrite(pins[0], LOW);
+//         digitalWrite(pins[1], HIGH);
+//         digitalWrite(pins[2], HIGH);
+//         digitalWrite(pins[3], LOW);
+//     } else {
+//         digitalWrite(pins[0], HIGH);
+//         digitalWrite(pins[1], LOW);
+//         digitalWrite(pins[2], HIGH);
+//         digitalWrite(pins[3], LOW);
+//     }
+// }
 
-   int ms_1 = 50000;
-   int wait_time = 100*ms_1;
-   int step = 0;
+// int main(){
 
-   while(1) {
-        stepMotor(step, pins);
-        step += 1;
-        wait(wait_time);
-   }
-}
+//    setup(); //init IO
+
+//    int ms_1 = 50000;
+//    int wait_time = 100*ms_1;
+//    int step = 0;
+
+//    while(1) {
+//         stepMotor(step, pins);
+//         step += 1;
+//         digitalWrite(3, HIGH);
+//         wait(wait_time);
+//    }
+// }
