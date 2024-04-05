83c78399685782c2dad9a1c7ec2c003401ac1ab3
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-04-05 10:46:36 -0400

    fixed constraints

--- Processor/FPGAWrapper.v
+++ Processor/FPGAWrapper.v
@@ -39,15 +39,12 @@ module FPGAWrapper (CLK100MHZ, CPU_RESETN, LED, PINS);
 
 
 	wire CLK;
-
 	tff clock_tff(CLK, 1'b1, CLK100MHZ, 1'b0);
 
 	assign LED[3:0] = instAddr[3:0];
 	
 	assign LED[4] = CLK;
 	
-	
-
 
 	// ADD YOUR MEMORY FILE HERE
 	localparam INSTR_FILE = "processor_tests";
