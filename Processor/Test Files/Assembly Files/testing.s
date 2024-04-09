cd2dfbbdfebaf5c70eaf89fdbc2ca059dfd4d90f
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-04-09 13:51:07 -0400

    set register 3 to be microsecond clock

--- Processor/Test Files/Assembly Files/testing.s
+++ Processor/Test Files/Assembly Files/testing.s
@@ -1,23 +1,74 @@
 .text
 			# Initialize Valuesaddi $3, $0, 1	# r3 = 1
-# read from pins 0-7
-addi $28, $0, 0
+addi $28, $0, 9
 addi $29, $29, 2047
+addi $8, $0, 0
+sw $8, 0($0)
+addi $9, $0, 1
+sw $9, 1($0)
+addi $10, $0, 2
+sw $10, 2($0)
+addi $11, $0, 3
+sw $11, 3($0)
+addi $12, $0, 0
+sw $8, 4($0)
+sw $9, 5($0)
+sw $10, 6($0)
+sw $11, 7($0)
+sw $12, 8($0)
 jal main
 add $8, $0, $2
-j "6"
+j "16"
 pinMode:
-	addi $10, $0, 12288
-	add $10, $10, $4
-	sw $5, 0($10)
-	"1":
-	jr $31
+addi $10, $0, 12288
+add $10, $10, $4
+sw $5, 0($10)
+"1":
+jr $31
 digitalWrite:
-	addi $10, $0, 4096
-	add $10, $10, $4
-	sw $5, 0($10)
-	"4":
-	jr $31
+addi $10, $0, 4096
+add $10, $10, $4
+sw $5, 0($10)
+"4":
+jr $31
+digitalRead:
+addi $9, $0, 4096
+add $9, $9, $4
+lw $2, 0($9)
+"7":
+jr $31
+stepMotor:
+addi $9, $0, 22185
+addi $10, $0, 3
+and $10, $4, $10
+addi $11, $0, 0
+"12":
+blt $11, $4, "13"
+j "15"
+"13":
+sra $12, $9, 4
+add $9, $0, $12
+"14":
+addi $11, $11, 1
+j "12"
+"15":
+addi $12, $0, 1
+and $12, $9, $12
+sra $13, $9, 1
+addi $14, $0, 1
+and $14, $13, $14
+sra $13, $9, 2
+addi $15, $0, 1
+and $15, $13, $15
+sra $13, $9, 3
+addi $16, $0, 1
+and $16, $13, $16
+sw $12, 4096($0)
+sw $14, 4097($0)
+sw $15, 4098($0)
+sw $16, 4099($0)
+"10":
+jr $31
 main:
 addi $8, $0, 0
 addi $9, $0, 1
@@ -28,15 +79,6 @@ sw $31, 0($29)
 jal pinMode
 lw $31, 0($29)
 addi $29, $29, 1
-addi $8, $0, 0
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal digitalWrite
-lw $31, 0($29)
-addi $29, $29, 1
 addi $8, $0, 1
 addi $9, $0, 1
 add $4, $0, $8
@@ -44,15 +86,6 @@ add $5, $0, $9
 addi $29, $29, -1
 sw $31, 0($29)
 jal pinMode
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 1
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal digitalWrite
 lw $31, 0($29)
 addi $29, $29, 1
 addi $8, $0, 2
@@ -62,6 +95,33 @@ add $5, $0, $9
 addi $29, $29, -1
 sw $31, 0($29)
 jal pinMode
+lw $31, 0($29)
+addi $29, $29, 1
+addi $8, $0, 3
+addi $9, $0, 1
+add $4, $0, $8
+add $5, $0, $9
+addi $29, $29, -1
+sw $31, 0($29)
+jal pinMode
+lw $31, 0($29)
+addi $29, $29, 1
+addi $8, $0, 0
+addi $9, $0, 0
+add $4, $0, $8
+add $5, $0, $9
+addi $29, $29, -1
+sw $31, 0($29)
+jal digitalWrite
+lw $31, 0($29)
+addi $29, $29, 1
+addi $8, $0, 1
+addi $9, $0, 0
+add $4, $0, $8
+add $5, $0, $9
+addi $29, $29, -1
+sw $31, 0($29)
+jal digitalWrite
 lw $31, 0($29)
 addi $29, $29, 1
 addi $8, $0, 2
@@ -74,96 +134,32 @@ jal digitalWrite
 lw $31, 0($29)
 addi $29, $29, 1
 addi $8, $0, 3
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal pinMode
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 3
-addi $9, $0, 1
+addi $9, $0, 0
 add $4, $0, $8
 add $5, $0, $9
 addi $29, $29, -1
 sw $31, 0($29)
 jal digitalWrite
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 4
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal pinMode
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 4
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal digitalWrite
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 5
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal pinMode
 lw $31, 0($29)
 addi $29, $29, 1
 addi $8, $0, 0
+"19":
 addi $9, $0, 1
+bne $9, $0, "20"
+j "17"
+"20":
 add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
+addi $29, $29, -2
 sw $31, 0($29)
-jal digitalWrite
+sw $9, 1($29)
+jal stepMotor
 lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 6
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal pinMode
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 0
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal digitalWrite
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 7
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal pinMode
-lw $31, 0($29)
-addi $29, $29, 1
-addi $8, $0, 0
-addi $9, $0, 1
-add $4, $0, $8
-add $5, $0, $9
-addi $29, $29, -1
-sw $31, 0($29)
-jal digitalWrite
-lw $31, 0($29)
-addi $29, $29, 1
-"7":
+lw $8, 1($29)
+addi $29, $29, 2
+addi $9, $10, 1
+add $10, $0, $9
+j "19"
+"17":
 jr $31
-"6":
+"16":
 add $0, $0, $0
--- Processor/components/memory.v
+++ Processor/components/memory.v
@@ -151,6 +151,116 @@ module counter32(Q, clk, reset);
     tff tff4(Q[4], inb34, clk, reset);
 endmodule
 
+module counter_6bit(Q, clk, reset);
+    input clk, reset;
+    output[5:0] Q;
+
+    wire inb12, inb23, inb34, inb45;
+
+    tff tff0(Q[0], 1'b1, clk, reset);
+    tff tff1(Q[1], Q[0], clk, reset);
+    and(inb12, Q[0], Q[1]);
+    tff tff2(Q[2], inb12, clk, reset);
+    and(inb23, inb12, Q[2]);
+    tff tff3(Q[3], inb23, clk, reset);
+    and(inb34, inb23, Q[3]);
+    tff tff4(Q[4], inb34, clk, reset);
+    and(inb45, inb34, Q[4]);
+    tff tff5(Q[5], inb45, clk, reset);
+endmodule
+
+module counter_32bit(Q, clk, reset);
+    input clk, reset;
+    output[31:0] Q;
+
+    wire inb12, inb23, inb34, inb45, inb56, inb6, inb78, inb89, inb910, inb1011, inb1112, inb1213, inb1314, inb1415, inb1516, inb1617, inb1718, inb1819, inb1920, inb2021, inb2122, inb2223, inb2324, inb2425, inb2526, inb2627, inb2728, inb2829, inb2930, inb3031;
+
+    tff tff0(Q[0], 1'b1, clk, reset);
+    tff tff1(Q[1], Q[0], clk, reset);
+    and(inb12, Q[0], Q[1]);
+    tff tff2(Q[2], inb12, clk, reset);
+    and(inb23, inb12, Q[2]);
+    tff tff3(Q[3], inb23, clk, reset);
+    and(inb34, inb23, Q[3]);
+    tff tff4(Q[4], inb34, clk, reset);
+    and(inb45, inb34, Q[4]);
+    tff tff5(Q[5], inb45, clk, reset);
+    and(inb56, inb45, Q[5]);
+    tff tff6(Q[6], inb56, clk, reset);
+    and(inb67, inb56, Q[6]);
+    tff tff7(Q[7], inb67, clk, reset);
+    and(inb78, inb67, Q[7]);
+    tff tff8(Q[8], inb78, clk, reset);
+    and(inb89, inb78, Q[8]);
+    tff tff9(Q[9], inb89, clk, reset);
+    and(inb910, inb89, Q[9]);
+    tff tff10(Q[10], inb910, clk, reset);
+    and(inb1011, inb910, Q[10]);
+    tff tff11(Q[11], inb1011, clk, reset);
+    and(inb1112, inb1011, Q[11]);
+    tff tff12(Q[12], inb1112, clk, reset);
+    and(inb1213, inb1112, Q[12]);
+    tff tff13(Q[13], inb1213, clk, reset);
+    and(inb1314, inb1213, Q[13]);
+    tff tff14(Q[14], inb1314, clk, reset);
+    and(inb1415, inb1314, Q[14]);
+    tff tff15(Q[15], inb1415, clk, reset);
+    and(inb1516, inb1415, Q[15]);
+    tff tff16(Q[16], inb1516, clk, reset);
+    and(inb1617, inb1516, Q[16]);
+    tff tff17(Q[17], inb1617, clk, reset);
+    and(inb1718, inb1617, Q[17]);
+    tff tff18(Q[18], inb1718, clk, reset);
+    and(inb1819, inb1718, Q[18]);
+    tff tff19(Q[19], inb1819, clk, reset);
+    and(inb1920, inb1819, Q[19]);
+    tff tff20(Q[20], inb1920, clk, reset);
+    and(inb2021, inb1920, Q[20]);
+    tff tff21(Q[21], inb2021, clk, reset);
+    and(inb2122, inb2021, Q[21]);
+    tff tff22(Q[22], inb2122, clk, reset);
+    and(inb2223, inb2122, Q[22]);
+    tff tff23(Q[23], inb2223, clk, reset);
+    and(inb2324, inb2223, Q[23]);
+    tff tff24(Q[24], inb2324, clk, reset);
+    and(inb2425, inb2324, Q[24]);
+    tff tff25(Q[25], inb2425, clk, reset);
+    and(inb2526, inb2425, Q[25]);
+    tff tff26(Q[26], inb2526, clk, reset);
+    and(inb2627, inb2526, Q[26]);
+    tff tff27(Q[27], inb2627, clk, reset);
+    and(inb2728, inb2627, Q[27]);
+    tff tff28(Q[28], inb2728, clk, reset);
+    and(inb2829, inb2728, Q[28]);
+    tff tff29(Q[29], inb2829, clk, reset);
+    and(inb2930, inb2829, Q[29]);
+    tff tff30(Q[30], inb2930, clk, reset);
+    and(inb3031, inb2930, Q[30]);
+    tff tff31(Q[31], inb3031, clk, reset);
+endmodule
+
+module mod_50_counter(Q, clk, reset);
+    input clk, reset;
+    output[5:0] Q;
+
+    wire reset_50;
+
+    assign reset_50 = reset | (Q == 6'b110010);
+
+    counter_6bit counter(Q, clk, reset_50);
+endmodule
+
+module us_counter_32bit(Q, clk, reset);
+    input clk, reset;
+    output[31:0] Q;
+
+    wire[5:0] counter;
+    mod_50_counter mod_50(counter, clk, reset);
+    wire incr = counter == 50;
+
+    counter_32bit counter_32(Q, incr, reset);
+endmodule
+
 module io_pin(out, pin, in, val_we, mode_we, clk);
     // mode: 0 = input, 1 = output
     input clk, val_we, mode_we, in;
