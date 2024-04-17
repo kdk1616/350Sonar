f3870e617a86a47884575e4a935df2b44f4facd7
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-04-16 20:45:49 -0400

    fixed resetting of wordreceiver

--- Processor/wordReceiver.v
+++ Processor/wordReceiver.v
@@ -16,14 +16,14 @@ module wordReceiver(ready, out, addr, clk, dataOnPin, dataPin, reset);
     wire update = dataOnPin | reset;
 
     always @(posedge update) begin
-        counter_val = (counter_val + 1) & {~reset, ~reset, ~reset, ~reset, ~reset};
-        ready_val = (counter_val == 5'b0) & ~reset;
+        counter_val = reset ? 0 : counter_val + 1;
+        ready_val = reset ? 0 : counter_val == 5'b0;
     end
 
     wire addr_update = ~ready_val | reset;
 
     always @(posedge addr_update) begin
-        addr_val <= (addr_val + 1) & ~reset;
+        addr_val <= reset ? -1 : addr_val + 1;
     end
 
     wire[31:0] next_reg = {dataPin, out[31:1]};
