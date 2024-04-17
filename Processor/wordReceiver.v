05feebcbb2dda4ebc1120c8378d89b92378bc9ce
Ethan Horowitz <ethan.horowitz@duke.edu>
2024-04-16 20:33:45 -0400

    wordreceiver works in gtkwave

--- Processor/wordReceiver.v
+++ Processor/wordReceiver.v
@@ -6,26 +6,27 @@ module wordReceiver(ready, out, addr, clk, dataOnPin, dataPin, reset);
     input dataOnPin, reset;
     input dataPin, clk;
 
-    reg [11:0] addr_val;
+    reg [11:0] addr_val = -1;
 
     assign addr = addr_val;
-    reg[4:0] counter_val;
-    reg ready = 0;
+    reg[4:0] counter_val = 0;
+    reg ready_val = 0;
+    assign ready = ready_val;
 
     wire update = dataOnPin | reset;
 
     always @(posedge update) begin
-        counter_val = (counter_val + 1) & ~reset;
-        ready = (counter_val == 5'b11111) & ~reset;
+        counter_val = (counter_val + 1) & {~reset, ~reset, ~reset, ~reset, ~reset};
+        ready_val = (counter_val == 5'b0) & ~reset;
     end
 
-    wire addr_update = ready | reset;
+    wire addr_update = ~ready_val | reset;
 
     always @(posedge addr_update) begin
         addr_val <= (addr_val + 1) & ~reset;
     end
 
-    wire next_reg = {dataPin, out[31:1]};
+    wire[31:0] next_reg = {dataPin, out[31:1]};
     
     register32 out_reg(.out(out), .clk(dataOnPin), .input_enable(1'b1), .in(next_reg), .reset(reset));
 endmodule
